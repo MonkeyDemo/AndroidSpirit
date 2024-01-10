4813fd50d66f1f3d5802c2bae0d5fa92800d9d56
MonkeyDemo <1257678683@qq.com>
2024-01-10 22:43:32 +0800

    first commit (第一次提交)

---
+++ app/src/main/java/com/monkeydemo/androidspirit/SpashActivity.kt
@@ -0,0 +1,11 @@
+package com.monkeydemo.androidspirit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spash)
+    }
+}
